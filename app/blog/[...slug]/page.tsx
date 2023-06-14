cb9617705443e8854e674630d794fdbe46ead1fd
cupid20103 <webphantom20103@gmail.com>
2023-06-14 13:23:58 -0700

    fix: update slug functions of router

--- app/blog/[...slug]/page.tsx
+++
@@ -1,4 +0,0 @@
-// Route	                    Example     URL	params
-// app/shop/[...slug]/page.js	/shop/a	    { slug: ['a'] }
-// app/shop/[...slug]/page.js	/shop/a/b	{ slug: ['a', 'b'] }
-// app/shop/[...slug]/page.js	/shop/a/b/c	{ slug: ['a', 'b', 'c'] }
